5c36bca983dd919c0840a6d7fc0cde90ecd2931a
xyqjq <1397995739@qq.com>
2021-07-02 00:52:09 +0800

    轮播图滚动动画优化

--- 2015_spring/task/task0002/task0002_3.js
+++ 2015_spring/task/task0002/task0002_3.js
@@ -1,27 +1,37 @@
 let inter = null;
-let current = 0;
+let current = 1;
 var red = null;
 let isloop = true;    
-var time = 2000;   
+var time = 5000;
+var preview = 0;   
 function change(red){
-    let list= $("#list").children(); 
-    $("img").css("left","-200px");
-    list[current].style.left="auto";
+    let list= $("#list").children();
+    // list[current].style.left="200px";
     console.log(current);
     if(red == "right"){
+        list[current].style.left="125px";
+        $("#"+list[preview].id).animate({left:"-200px"},"slow","linear");
+        $("#"+list[current].id).animate({left:""},"slow","linear");
         if(current==list.length-1 && !eval(isloop)){
             clearInterval(inter);
         }else if(current==list.length-1){
+            preview = current;
             current=0;
         }else{
+            preview = current;
             current++;
         }   
     }else{
+        list[current].style.left="-125px";
+        $("#"+list[preview].id).animate({left:"200px"},"slow","linear");
+        $("#"+list[current].id).animate({left:""},"slow","linear");
         if(current==0  && !eval(isloop)){
             clearInterval(inter);
         }else if(current==0){
+            preview = current;
             current=list.length-1;
         }else{
+            preview = current;
             current--;
         }  
     }
@@ -29,10 +39,10 @@ function change(red){
 window.onload=function(){
     let list= $("#list").children();
     red = "right";    
-    list[current].style.left="auto";
     for(let i=0;i<list.length;i++){
         $("#title").append("<i>"+i+"</i>");
     }
+    list[preview].style.left="0px"; 
     inter = setInterval(function(){change(red)},time);
 }
 
