0c85b1788a9c0d0394db82b7c14b3f353fe4950f
qjq <1397995739@qq.com>
2021-07-05 13:04:14 +0800

    输入提示框完成

--- 2015_spring/task/task0002/task0002_4.js
+++ 2015_spring/task/task0002/task0002_4.js
@@ -2,6 +2,7 @@ var suggestData = ['Simon', 'Erik', 'Kener'];
 var old_value = "";
 var current_value = "";
 var list = [];
+var highLight = -1;
 $(function(){
     old_value = $("#searchText").val();
     $("#searchText").focus(
@@ -10,16 +11,59 @@ $(function(){
         }
     );
     $("#searchText").keyup(
-        function(){
-            autoCompelete(suggestData);
+        function(event){
+            if(event.keyCode == 38){
+                console.log(38);
+                if($("#auto").is(":visible")){
+                    $("#"+highLight).css("background-color","white");
+                    if(highLight > 0){
+                        
+                        highLight--;
+                    }else{
+                        highLight = $("#auto").children().length - 1;
+                    }
+                    $("#"+highLight).css("background-color","aliceblue");
+                    console.log(highLight);
+                }
+            }else if(event.keyCode == 40){
+                console.log(40);
+                if($("#auto").is(":visible")){
+                    $("#"+highLight).css("background-color","white");
+                    if(highLight<$("#auto").children().length-1){                       
+                        highLight++;
+                    }else{
+                        highLight = 0;  
+                    }
+                    $("#"+highLight).css("background-color","aliceblue");
+                    console.log(highLight);
+                }
+            }else if(event.keyCode == 13){
+                if($("#auto").is(":visible")){
+                    $("#searchText").val($("#"+highLight).text());
+                    $("#auto").css("display","none");
+                    highLight = -1;
+                }
+            }else{
+                autoCompelete(suggestData);
+            }
         }
     );
 });
 
+//点击页面隐藏自动补全提示框
+document.onclick = function (e) {
+    var tar = e.target;
+    if (tar.id != "searchText") {
+        if ($("#auto").is(":visible")) {
+            $("#auto").css("display", "none")
+        }
+    }
+}
+
 function autoCompelete(suggestData){
     current_value = $("#searchText").val();
     console.log(old_value +":"+ current_value);
-    if(old_value != current_value){
+    if(old_value != current_value || current_value == ""){
         list = [];
         for(i in suggestData){
             if(suggestData[i].toLowerCase().indexOf(current_value.toLowerCase())>-1){
@@ -28,10 +72,24 @@ function autoCompelete(suggestData){
         }
         if(list.length > 0){
             $("#auto").empty();
+            $("#auto").css("display","block");
             for(j in list){
-                $("#auto").append("<div>"+list[j]+"</div>"); 
+                $("#auto").append("<div id="+j+">"+list[j]+"</div>");
+                $("#"+j).mouseover(function(){
+                    $("#"+highLight).css("background-color","white");
+                    $(this).css("background-color","aliceblue");
+                    highLight = $(this).attr("id");
+                    console.log($(this).attr("id"));
+                });
+                $("#"+j).mouseout(function(){
+                    $(this).css("background-color","white");
+                });
+                $("#"+j).click(function(){
+                    $("#searchText").val($("#"+highLight).text());
+                    $("#auto").css("display","none");
+                    highLight = -1;
+                })  
             }
-            $("#auto").css("display","block")
         }
     }else{
 
